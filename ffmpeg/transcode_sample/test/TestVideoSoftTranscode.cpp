1f0cee2133b3be9664adac65001b1df75131ae03
zimengyu1992 <649822851@qq.com>
2021-11-16 15:23:14 +0800

    转码性能测试

--- ffmpeg/transcode_sample/test/TestVideoSoftTranscode.cpp
+++ ffmpeg/transcode_sample/test/TestVideoSoftTranscode.cpp
@@ -3,31 +3,53 @@
 using namespace std;
 
 #include "StreamReader.h"
+#include "StreamWriter.h"
+
 #include "VideoTranscode.h"
 #include "VideoSoftTranscode.h"
+#include "VideoIntelQsvTranscode.h"
+
+#define LOG_TAGS "app_main::"
+
+#define LOGD(...) av_log(NULL, AV_LOG_DEBUG, __VA_ARGS__) //Log().d
+#define LOGI(...) av_log(NULL, AV_LOG_INFO, __VA_ARGS__)  //Log().i
+#define LOGE(...) av_log(NULL, AV_LOG_ERROR, __VA_ARGS__) //Log().e
 
 int main(int argc, char *argv[])
 {
     cout << "transcode" << endl;
+    if (argc != 3)
+    {
+        cout << "usage rtmp:// rtmp://" << endl;
+        return 0;
+    }
+    std::string inputDataSource = argv[1];
+    std::string outputDataSource = argv[2];
+    //av_log_set_level(AV_LOG_DEBUG);
+    av_log_set_level(AV_LOG_INFO);
 
-    av_log_set_level(AV_LOG_DEBUG);
-    //av_log_set_level(AV_LOG_INFO);
-    AVCodecParameters *videoCodecpar;
-    AVCodecParameters *audioCodecpar;
-    StreamReader reader;
-    reader.setDataSource("rtmp://127.0.0.1:1935/live/Test", "flv");
-    reader.open();
-
+    bool outputOpen;
     VideoSoftTranscode transcode;
+    StreamWriter writer;
+    StreamReader reader;
+
+    AVCodecParameters *videoCodecpar = nullptr;
+    AVCodecParameters *audioCodecpar = nullptr;
 
     VideoDecodeParam decodeParam;
+    VideoEncodeParam encodeParam;
+
+    AVPacket *packet = nullptr;
+
+    reader.setDataSource(inputDataSource.c_str(), "flv");
+    reader.open();
+
     decodeParam.setCodecId(AV_CODEC_ID_H264);
     transcode.setDecodeParam(decodeParam);
 
-    VideoEncodeParam encodeParam;
     encodeParam.setEncodeMode(VIDEO_ENCODE_STREAM);
-    encodeParam.setEncodeIFrame(true);
-    encodeParam.setCodecId(AV_CODEC_ID_HEVC);
+    encodeParam.setEncodeIFrame(false);
+    encodeParam.setCodecId(AV_CODEC_ID_H264);
     encodeParam.setBitrate(500);
     encodeParam.setVideoSize(VideoSize{352, 288});
     encodeParam.setFramerate(25);
@@ -37,39 +59,66 @@ int main(int argc, char *argv[])
     transcode.setEncodeParam(encodeParam);
 
     transcode.open();
+    outputOpen = false;
 
-    AVPacket *packet;
     packet = av_packet_alloc();
-
-    FILE *pTotalFile = fopen("es_total.h265", "ab");
-
     while (true)
     {
-        while (transcode.receivePacket(packet) == 0)
+        if (transcode.readyForReceive())
         {
-            printf("push video id:%d dts:%lld pts:%lld.\n", packet->stream_index, packet->dts, packet->pts);
-            fwrite(packet->data, 1, packet->size, pTotalFile);
-            av_packet_unref(packet);
-        }
+            if (!outputOpen)
+            {
+                writer.setDataSource(outputDataSource.c_str(), "flv");
 
+                videoCodecpar = avcodec_parameters_alloc();
+                audioCodecpar = avcodec_parameters_alloc();
+                reader.getAudioCodecPar(audioCodecpar);
+                transcode.getVideoCodecPar(videoCodecpar);
+
+                writer.setAudioCodecPar(audioCodecpar);
+                writer.setVideoCodecPar(videoCodecpar);
+
+                writer.open();
+
+                avcodec_parameters_free(&audioCodecpar);
+                avcodec_parameters_free(&videoCodecpar);
+                outputOpen = true;
+            }
+
+            if (outputOpen)
+            {
+                packet = av_packet_alloc();
+                while (transcode.receivePacket(packet) == 0)
+                {
+                    packet->stream_index = writer.getVideoStream()->index;
+                    //printf("push video id:%d dts:%lld pts:%lld.\n", packet->stream_index, packet->dts, packet->pts);
+                    writer.writePacket(packet);
+                    av_packet_unref(packet);
+                }
+                av_packet_free(&packet);
+            }
+        }
+        packet = av_packet_alloc();
         if (reader.readPacket(packet) == 0)
         {
             if (packet->stream_index == reader.getVideoStream()->index)
             {
-                // packet->stream_index = writer.getVideoStream()->index;
-                // printf("push video id:%d pts:%lld.\n", packet->stream_index, packet->pts);
-                // writer.writePacket(packet);
+                //printf("transcode::sendPacket\n");
                 transcode.sendPacket(packet);
             }
+            else if (packet->stream_index == reader.getAudioStream()->index)
+            {
+                if (outputOpen)
+                {
+                    packet->stream_index = writer.getAudioStream()->index;
+                    writer.writePacket(packet);
+                }
+            }
         }
-
-        av_packet_unref(packet);
+        av_packet_free(&packet);
     }
-
-    fclose(pTotalFile);
-
-    av_packet_free(&packet);
-    transcode.close();
     reader.close();
+    transcode.close();
+    writer.close();
     return 0;
 }
